b8a9d1edf6ec1c351c22df7171fa90ae90715c8b
Guest User <Guest@epicodus-3.local>
2017-09-05 12:02:04 -0700

    add new Espresso tests

--- app/src/main/java/com/epicodus/annatimofeyeva/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/annatimofeyeva/myrestaurants/MainActivity.java
@@ -1,18 +1,21 @@
 package com.epicodus.annatimofeyeva.myrestaurants;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
 
 
@@ -22,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrichregular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
                 intent.putExtra("location", "Here are all the restaurants near: " + location);
                 startActivity(intent);
+
+
 
             }
         });
